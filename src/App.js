9d17914024bbc3414db041af8f6f3778bbd45e6a
Blair <jiqiang.zhu01@gmail.com>
2020-11-05 16:37:20 +0800

    [react]React-redux Summary Demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Num：{this.props.value}</h1>
+        <button onClick={this.props.add}>值+1</button>
+        <button onClick={this.props.addNum}>值+5</button>
+      </div>
+    )
+  }
+}
+function reducer(state = { num: 0 }, action) {
+  if (action.type.indexOf("redux") === -1) {
+    state = ActionFn[action.type](state, action);
+    return { ...state }
+  } else {
+    return state;
+  }
+}
+let store = createStore(reducer);
+const ActionFn = {
+  add: (state, action) => {
+    state.num++;
+    return state;
+  },
+  addNum: (state, action) => {
+    state.num += action.num;
+    return state;
+  }
+}
+function mapStateToProps(state) {
+  return {
+    value: state.num
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    add: () => { dispatch({ type: 'add' }) },
+    addNum: () => { dispatch({ type: 'addNum', num: 5 }) }
+  }
+}
+
+const Ap = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <Ap></Ap>
+        </Provider>
+      </div>
+    )
+  }
 }
 
 export default App;
